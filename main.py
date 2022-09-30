f5df3c0df0c67262d2ea59893ed010b3abd3087f
Russell Stanley <russell@ausocean.org>
2022-10-01 09:02:27 +0930

    main.py: add handling for single files or and entire dir

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import sys
 import time
 import starfield
 import noise
+import glob
 
 # DVX Format
 #t(Unix), x, y, p
 
 # Load event file path
 if (len(sys.argv) > 1):
-    file_path = sys.argv[1]
+    path = sys.argv[1]
+    if path.endswith(".raw"):
+        # Single file
+        files = [path]
+    else:
+        # Add directory
+        files = glob.glob(path + "/*.raw")
 else:
     exit()
 
@@ -21,7 +28,8 @@ if (len(sys.argv) > 2):
 
 start_time = time.time()
 
-player = starfield.csv_to_starfield(file_path)
-data = player.generate_star_field(30*starfield.ONE_SECOND, file_path[:len(file_path) - 4] + ".jpg", hot_pixels_path=noise.FILE_PATH)
+for file_path in files:
+    player = starfield.csv_to_starfield(file_path)
+    data = player.generate_star_field(30*starfield.ONE_SECOND, file_path[:len(file_path) - 4] + ".jpg", hot_pixels_path=noise.FILE_PATH)
 
 print("total: %s sec" % (time.time() - start_time))
